5494e7d2c8dcbf524a286fd51e386c65edea5887
Meher Vamsi Dontoju <75484855+Mehervamsi1@users.noreply.github.com>
2025-09-11 14:00:48 -0400

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 # app.py - Main Flask Application
 import os
-import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
